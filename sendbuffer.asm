f1ea90410f2ece3048a9efb3d4dffc11075fed3b
Shiun Robot DIY <masonchen1003@gmail.com>
2024-06-02 08:33:20 +0800

    Update sendbuffer.asm

--- sendbuffer.asm
+++ sendbuffer.asm
@@ -9,51 +9,54 @@ sendBufferAsm:
     ; Example: r5 = length, r4 = buffer start address
     
     ; Set GPIO 12 as output
-    ldr r0, =0x40014000  ; Load SIO base address
-    ldr r1, =0x1000      ; GPIO 12 set (1 << 12)
-    str r1, [r0, #0x04]  ; Set GPIO 12 direction to output
+    ldr r0, =SIO_BASE      ; Load SIO base address
+    ldr r1, =GPIO_12_MASK  ; GPIO 12 set (1 << 12)
+    str r1, [r0, #0x04]    ; Set GPIO 12 direction to output
 
     ; Load addresses of the set and clear registers
-    ldr r2, =0x4001401C  ; GPIO_OUT_CLR
-    ldr r3, =0x40014018  ; GPIO_OUT_SET
-    ldr r1, =0x1000      ; Mask for GPIO 12 (1 << 12)
+    ldr r2, =GPIO_OUT_CLR  ; GPIO_OUT_CLR
+    ldr r3, =GPIO_OUT_SET  ; GPIO_OUT_SET
+    ldr r1, =GPIO_12_MASK  ; Mask for GPIO 12 (1 << 12)
 
-    cpsid i              ; disable irq
+    cpsid i                ; disable irq
     
     b .start
     
-.nextbit:               ;            C0
-    str r1, [r3, #0]    ; pin := hi  C2
-    tst r7, r0          ;            C3
-    bne .islate         ;            C4
-    str r1, [r2, #0]    ; pin := lo  C6
+.nextbit:                 ;            C0
+    str r1, [r3, #0]      ; pin := hi  C2
+    tst r7, r0            ;            C3
+    bne .islate           ;            C4
+    str r1, [r2, #0]      ; pin := lo  C6
 .islate:
-    lsrs r7, r7, #1     ; r7 >>= 1   C7
-    bne .justbit        ;            C8
+    lsrs r7, r7, #1       ; r7 >>= 1   C7
+    bne .justbit          ;            C8
     
     ; not just a bit - need new byte
-    adds r4, #1         ; r4++       C9
-    subs r5, #1         ; r5--       C10
-    bcc .stop           ; if (r5<0) goto .stop  C11
+    adds r4, #1           ; r4++       C9
+    subs r5, #1           ; r5--       C10
+    bcc .stop             ; if (r5<0) goto .stop  C11
 .start:
-    movs r7, #0x80      ; reset mask C12
-    nop                 ;            C13
+    movs r7, #0x80        ; reset mask C12
+    nop                   ;            C13
 
-.common:               ;             C13
-    str r1, [r2, #0]   ; pin := lo   C15
+.common:                  ;             C13
+    str r1, [r2, #0]      ; pin := lo   C15
     ; always re-load byte - it just fits with the cycles better this way
-    ldrb r0, [r4, #0]  ; r0 := *r4   C17
-    b .nextbit         ;             C20
+    ldrb r0, [r4, #0]     ; r0 := *r4   C17
+    b .nextbit            ;             C20
 
 .justbit: ; C10
     ; no nops, branch taken is already 3 cycles
     b .common ; C13
 
 .stop:    
-    str r1, [r2, #0]   ; pin := lo
-    cpsie i            ; enable irq
+    str r1, [r2, #0]      ; pin := lo
+    cpsie i               ; enable irq
 
     pop {r4,r5,r6,r7,pc}
-    
-    ; Constants loaded using literal pool
-    .ltorg
+
+    ; Constants loaded using manual constant pool
+SIO_BASE:     .word 0x40014000
+GPIO_12_MASK: .word 0x00001000  ; (1 << 12)
+GPIO_OUT_CLR: .word 0x4001401C
+GPIO_OUT_SET: .word 0x40014018
